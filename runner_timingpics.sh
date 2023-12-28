759a55d92de89b10a2a1199b752abfef1850dfc5
Nerolation <toni_w@gmx.at>
2023-12-28 11:08:56 +0100

    update progress

--- runner_timingpics.sh
+++ runner_timingpics.sh
@@ -4,6 +4,7 @@
 ./bin/python3 run_ethdata_pipeline.py
 
 cd ./timing.pics
+cp ../last_updated.txt ./
 cp ../app.py ./
 cp ../missed_slot_over_time_chart.pkl ./
 cp ../time_in_slot_scatter_chart.pkl ./
@@ -13,6 +14,7 @@ cp ../Procfile ./
 cp ../runner_timingpics.sh ./
 
 git add app.py 
+git add last_updated.txt 
 git add missed_slot_over_time_chart.pkl 
 git add time_in_slot_scatter_chart.pkl 
 git add assets/
