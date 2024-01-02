79809a4cd48cb71d681fa630c61df012b6fa9e7f
Nerolation <toni_w@gmx.at>
2024-01-02 11:34:32 +0100

    update progress

--- runner_timingpics.sh
+++ runner_timingpics.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/ethereum/timing_games
-#./bin/python3 run_ethdata_pipeline.py
+./bin/python3 run_ethdata_pipeline.py
 ./bin/python3 build.py
 
 cd ./timing.pics
@@ -8,6 +8,13 @@ cp ../last_updated.txt ./
 cp ../app.py ./
 cp ../missed_slot_over_time_chart.pkl ./
 cp ../time_in_slot_scatter_chart.pkl ./
+cp ../gamer_bars.pkl ./
+cp ../missed_slot_bars.pkl ./
+cp ../gamer_advantage_lines.pkl ./
+cp ../gamer_advantage_avg.pkl ./
+cp ../missed_market_share_chart.pkl ./
+    
+    
 cp -r ../assets/ ./
 cp ../requirements.txt ./
 cp ../Procfile ./
@@ -17,6 +24,11 @@ git add app.py
 git add last_updated.txt 
 git add missed_slot_over_time_chart.pkl 
 git add time_in_slot_scatter_chart.pkl 
+git add gamer_bars.pkl 
+git add missed_slot_bars.pkl 
+git add gamer_advantage_lines.pkl 
+git add gamer_advantage_avg.pkl 
+git add missed_market_share_chart.pkl 
 git add assets/
 git add requirements.txt 
 git add Procfile 
