3ef0290fc28d21801a2ec772f615285cf6c2b77f
nerolation <info@toniwahrstaetter.com>
2024-02-26 19:57:20 +0100

    update progress

--- runner_timingpics.sh
+++ runner_timingpics.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/ethereum/timing_games_building
-./bin/python3 run_ethdata_pipeline.py
-./bin/python3 build.py
+../open-eth-pipes/bin/python3 run_ethdata_pipeline.py
+../open-eth-pipes/bin/python3 build.py
 
 cd ./timing.pics
 cp ../last_updated.txt ./
